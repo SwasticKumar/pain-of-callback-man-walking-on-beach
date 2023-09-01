408a0de31aa3a75b0b22f2026dc8c789c8871229
SwasticKumar <kswastic@gmail.com>
2023-09-01 20:58:43 +0530

    callback-hell

---
+++ script.js
@@ -0,0 +1,76 @@
+let displayContent = document.getElementById("root");
+
+setTimeout(() => {
+  displayContent.innerHTML = `<img class="" src="./image/step-1.png" >`;
+  setTimeout(() => {
+    displayContent.innerHTML = `<img class="" src="./image/step-2.png" >`;
+    setTimeout(() => {
+      displayContent.innerHTML = `<img class="" src="./image/step-3.png" >`;
+      setTimeout(() => {
+        displayContent.innerHTML = `<img class="four" src="./image/step-4.png" >`;
+        setTimeout(() => {
+          displayContent.innerHTML = `<img class="" src="./image/step-5.png" >`;
+          setTimeout(() => {
+            displayContent.innerHTML = `<img class="" src="./image/step-6.png" >`;
+            setTimeout(() => {
+              displayContent.innerHTML = `<img class="" src="./image/step-7.png">`;
+              setTimeout(() => {
+                displayContent.innerHTML = `<img class="eight" src="./image/step-8.png" >`;
+                // speed
+                setTimeout(() => {
+                  displayContent.innerHTML = `<img class="" src="./image/step-1.png" >`;
+                  setTimeout(() => {
+                    displayContent.innerHTML = `<img class="" src="./image/step-2.png" >`;
+                    setTimeout(() => {
+                      displayContent.innerHTML = `<img class="" src="./image/step-3.png" >`;
+                      setTimeout(() => {
+                        displayContent.innerHTML = `<img class="four" src="./image/step-4.png" >`;
+                        setTimeout(() => {
+                          displayContent.innerHTML = `<img class="" src="./image/step-5.png" >`;
+                          setTimeout(() => {
+                            displayContent.innerHTML = `<img class="" src="./image/step-6.png" >`;
+                            setTimeout(() => {
+                              displayContent.innerHTML = `<img class="" src="./image/step-7.png">`;
+                              setTimeout(() => {
+                                displayContent.innerHTML = `<img class="eight" src="./image/step-8.png" >`;
+                                //  Run
+                                setTimeout(() => {
+                                  displayContent.innerHTML = `<img class="" src="./image/step-1.png" >`;
+                                  setTimeout(() => {
+                                    displayContent.innerHTML = `<img class="" src="./image/step-2.png" >`;
+                                    setTimeout(() => {
+                                      displayContent.innerHTML = `<img class="" src="./image/step-3.png" >`;
+                                      setTimeout(() => {
+                                        displayContent.innerHTML = `<img class="four" src="./image/step-4.png" >`;
+                                        setTimeout(() => {
+                                          displayContent.innerHTML = `<img class="" src="./image/step-5.png" >`;
+                                          setTimeout(() => {
+                                            displayContent.innerHTML = `<img class="" src="./image/step-6.png" >`;
+                                            setTimeout(() => {
+                                              displayContent.innerHTML = `<img class="" src="./image/step-7.png">`;
+                                              setTimeout(() => {
+                                                displayContent.innerHTML = `<img class="eight" src="./image/step-8.png" >`;
+                                              }, 200);
+                                            }, 200);
+                                          }, 200);
+                                        }, 200);
+                                      }, 200);
+                                    }, 200);
+                                  }, 200);
+                                }, 200);
+                              }, 200);
+                            }, 200);
+                          }, 200);
+                        }, 200);
+                      }, 200);
+                    }, 200);
+                  }, 200);
+                }, 200);
+              }, 200);
+            }, 200);
+          }, 200);
+        }, 200);
+      }, 200);
+    }, 200);
+  }, 200);
+}, 200);
